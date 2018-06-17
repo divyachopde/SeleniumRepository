60169f4c0b665199755388ac81a196b6dc17a2e6
divyachopde <divyachopde89@gmail.com>
2018-06-17 12:30:09 +0530

    commenttest

---
+++ SeleniumJava/src/Session16June/ClassMain.java
@@ -0,0 +1,17 @@
+package Session16June;
+
+class ClassMain {
+
+public static void main (String args[]) {
+
+WaterFilter w = new WaterFilter();
+System.out.println(w.filterType);
+w.filterType="Normal";
+w.tap=true;
+
+System.out.println("FilterType is " + w.filterType);
+System.out.println(w.tap);
+System.out.println(w);
+}
+
+}
---
+++ SeleniumJava/src/Session16June/WaterFilter.java
@@ -0,0 +1,11 @@
+package Session16June;
+
+class WaterFilter{
+
+String filterType ;
+boolean tap;
+int capacity;
+String color;
+double price;
+
+}
