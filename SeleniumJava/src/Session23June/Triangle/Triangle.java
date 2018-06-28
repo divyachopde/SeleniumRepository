c20b78fc160508e474219739a93638d070a827e4
divyachopde <divyachopde89@gmail.com>
2018-06-29 00:02:34 +0530

    commit assignment 29june

---
+++ SeleniumJava/src/Session23June/Triangle/Triangle.java
@@ -0,0 +1,22 @@
+package Session23June.Triangle;
+
+public class Triangle {
+
+	int p1;
+	int p2;
+	int p3;
+
+	Triangle(int p1, int p2, int p3) {
+
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+
+	}
+
+	double calculatePerimeter() {
+
+		return (p1 + p2 + p3);
+	}
+
+}
---
+++ SeleniumJava/src/Session23June/Triangle/TriangleMain.java
@@ -0,0 +1,17 @@
+package Session23June.Triangle;
+
+public class TriangleMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Triangle T = new Triangle(2, 3, 4);
+
+		System.out.println("Perimeter of Triangle is : " + T.calculatePerimeter());
+
+	}
+
+}
