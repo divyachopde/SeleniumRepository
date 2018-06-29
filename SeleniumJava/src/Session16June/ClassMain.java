c834bffadc1bf0aa0a8b8ab49763660300119263
divyachopde <divyachopde89@gmail.com>
2018-06-29 10:02:02 +0530

    commit

--- SeleniumJava/src/Session16June/ClassMain.java
+++ SeleniumJava/src/Session16June/ClassMain.java
@@ -2,16 +2,19 @@ package Session16June;
 
 class ClassMain {
 
-public static void main (String args[]) {
+	public static void main(String args[]) {
 
-WaterFilter w = new WaterFilter();
-System.out.println(w.filterType);
-w.filterType="Normal";
-w.tap=true;
+		WaterFilter w = new WaterFilter();
+		System.out.println(w.filterType);
+		w.filterType = "Normal";
+		w.tap = true;
+		w.color="red";
 
-System.out.println("FilterType is " + w.filterType);
-System.out.println(w.tap);
-System.out.println(w);
-}
+		System.out.println("FilterType is " + w.filterType);
+		System.out.println(w.tap);
+		System.out.println(w);
+		
+		
+	}
 
 }
--- SeleniumJava/src/Session16June/WaterFilter.java
+++ SeleniumJava/src/Session16June/WaterFilter.java
@@ -1,11 +1,23 @@
 package Session16June;
 
-class WaterFilter{
+class WaterFilter {
 
-String filterType ;
-boolean tap;
-int capacity;
-String color;
-double price;
+	String filterType;
+	boolean tap;
+	int capacity;
+	String color;
+	double price;
+	@Override
+	public String toString() {
+		
+		return "WaterFilter [filterType=" + filterType + ", tap=" + tap + ", capacity=" + capacity + ", color=" + color
+				+ ", price=" + price + "]";
+	}
 
+	/*public String toString() {
+
+		return "color "+this.color;
+	}
+*/
+	
 }
